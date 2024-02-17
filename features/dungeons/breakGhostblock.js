59dd42a5748780a2e2f9f08d7a1804b360ce4eb7
PridedBacon <145861953+PridedBacon@users.noreply.github.com>
2024-02-17 21:17:53 +0100

    small fix

--- features/dungeons/breakGhostblock.js
+++ features/dungeons/breakGhostblock.js
@@ -16,10 +16,9 @@ registerWhen(
         for (let i = 0; i < blocks.length; i++) {
             let [call, block] = blocks[i];
             blocks[i][0]++;
-            if (call > 20) blocks.splice(i, 1);
+            if (call > 30) blocks.splice(i, 1);
             if (World.getBlockAt(block.x, block.y, block.z).type.getID() !== 0) {
                 World.getWorld().func_175698_g(block.toMCBlock());
-                blocks.splice(i, 1);
             }
         }
     }),
