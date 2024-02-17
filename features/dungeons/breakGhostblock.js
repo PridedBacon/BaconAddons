2db93076f4c3774f260799bcbf283b45b755b366
PridedBacon <145861953+PridedBacon@users.noreply.github.com>
2024-02-17 21:56:28 +0100

    moved to Render in breakGhostblock.js

--- features/dungeons/breakGhostblock.js
+++ features/dungeons/breakGhostblock.js
@@ -5,18 +5,18 @@ let blocks = [];
 
 registerWhen(
     register("blockBreak", (block) => {
-        blocks.push([0, block.pos]);
+        blocks.push([Date.now(), block.pos]);
     }),
     () => Config.enableBreakGhostblock,
     "Dungeon"
 );
 
 registerWhen(
-    register("tick", () => {
+    register("renderWorld", () => {
         for (let i = 0; i < blocks.length; i++) {
-            let [call, block] = blocks[i];
+            let [date, block] = blocks[i];
             blocks[i][0]++;
-            if (call > 30) blocks.splice(i, 1);
+            if (Date.now() - date > 1500) blocks.splice(i, 1);
             if (World.getBlockAt(block.x, block.y, block.z).type.getID() !== 0) {
                 World.getWorld().func_175698_g(block.toMCBlock());
             }
